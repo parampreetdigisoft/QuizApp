ec60de2836379b1c3bbaee03cc968ef6283eb683
Aksh Pyal <Aksh Pyal>
2025-12-03 10:32:48 +0530

    commit for the folder structure changes

--- Back-end/QuizApp.UI/Models/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuizApp.UI.Models
-{
-    public class LoginViewModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-}
--- QuizApp.UI/src/environments/environment.development.ts
+++ QuizApp.UI/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    baseUrl: "https://localhost:44302",
+    baseUrl: "http://localhost:5029",
     endpoints: {
         login: "api/user/login",
         regiser: "api/user/register",
@@ -9,7 +9,7 @@ export const environment = {
         questionEndpoint: "api/question",
         allAnswers: "api/useranswers/all",
         allUsers: "api/user/all",
-        userEndpoint:"api/user",
+        userEndpoint: "api/user",
         answerEndpoint:"api/answeroption",
         quizEndpoint:"api/quiz",
         quizAttemptEndpoint:"api/quizAttempt"
